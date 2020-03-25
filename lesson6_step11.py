2b29f4347a830abe4814ee15576a8c4de117093e
ramaxadillinger <ramaxadillinger@gmail.com>
2020-03-25 14:11:53 +0200

    интересный код (нет)

---
+++ lesson6_step11.py
@@ -0,0 +1,21 @@
+﻿from selenium import webdriver
+import os
+import time
+with open('file.txt', 'w') as file:
+    file.write("Hello Selenium!!!")
+pth = os.path.abspath("file.txt")
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/file_input.html")
+firstname = "Ivan"
+lastname = "Ivanov"
+email = "Ivanov.Ivan@gmail.com"
+
+browser.find_element_by_name("firstname").send_keys(firstname)
+browser.find_element_by_name("lastname").send_keys(lastname)
+browser.find_element_by_name("email").send_keys(email)
+browser.find_element_by_css_selector("#file").send_keys(pth)
+browser.find_element_by_css_selector(".btn").click()
+
+time.sleep(3)
+
+browser.qiut
